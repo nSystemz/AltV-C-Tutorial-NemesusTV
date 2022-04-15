8adea7d22700f40ae5bf06622de8686d058c2a70
Franze, Mario <mario.franze@gmail.com>
2022-04-15 21:04:40 +0200

    Removed unused imports

--- resources/AltVTutorial/AltVTutorial/Datenbank.cs
+++ resources/AltVTutorial/AltVTutorial/Datenbank.cs
@@ -1,10 +1,6 @@
 ﻿using AltV.Net;
 using MySql.Data.MySqlClient;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AltVTutorial
 {
