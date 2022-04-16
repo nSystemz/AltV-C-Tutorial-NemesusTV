825fba9e7d4238e5fa1aa68709dae3b99f4c4378
Franze, Mario <mario.franze@gmail.com>
2022-04-16 10:48:53 +0200

    Added default values

--- resources/AltVTutorial/AltVTutorial/Database/Models/Base.Model.cs
+++ resources/AltVTutorial/AltVTutorial/Database/Models/Base.Model.cs
@@ -5,7 +5,7 @@ namespace AltVTutorial.Database.Models
     public class Base
     {
         public int Id { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public DateTime UpdatedAt { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
     }
 }
