8746583902158f6c20f51ab7c988e53d43d46111
Franze, Mario <mario.franze@gmail.com>
2022-04-16 02:51:41 +0200

    Added migration to missing password field on User

--- resources/AltVTutorial/AltVTutorial/Migrations/20220416005018_AddPasswordToUserModel.Designer.cs
+++ resources/AltVTutorial/AltVTutorial/Migrations/20220416005018_AddPasswordToUserModel.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using AltVTutorial.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AltVTutorial.Migrations
 {
     [DbContext(typeof(ConnectionContext))]
-    partial class ConnectionContextModelSnapshot : ModelSnapshot
+    [Migration("20220416005018_AddPasswordToUserModel")]
+    partial class AddPasswordToUserModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ resources/AltVTutorial/AltVTutorial/Migrations/20220416005018_AddPasswordToUserModel.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AltVTutorial.Migrations
+{
+    public partial class AddPasswordToUserModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "User",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "User");
+        }
+    }
+}
