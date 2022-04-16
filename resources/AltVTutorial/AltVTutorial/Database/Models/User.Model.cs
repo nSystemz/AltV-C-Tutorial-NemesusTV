bc980f2eb369bbd27af87c35a7a4e8b960c66982
Franze, Mario <mario.franze@gmail.com>
2022-04-16 02:51:14 +0200

    Added missing password field

--- resources/AltVTutorial/AltVTutorial/Database/Models/User.Model.cs
+++ resources/AltVTutorial/AltVTutorial/Database/Models/User.Model.cs
@@ -5,6 +5,7 @@ namespace AltVTutorial.Database.Models
     public class User : Base
     {
         public string Username { get; set; }
+        public string Password { get; set; }
         public string EMail { get; set; }
         public DateTime VerifiedAt { get; set; }
         public string Token { get; set; }
