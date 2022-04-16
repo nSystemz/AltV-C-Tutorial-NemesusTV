ea54d949fe5ee6dac09638aea042823a3899e963
Franze, Mario <mario.franze@gmail.com>
2022-04-16 10:50:57 +0200

    Refactored login even more

--- resources/AltVTutorial/AltVTutorial/GameEvents/Account/Login.cs
+++ resources/AltVTutorial/AltVTutorial/GameEvents/Account/Login.cs
@@ -1,6 +1,5 @@
 ﻿using AltV.Net;
 using AltV.Net.Enums;
-using AltV.Net.Resources.Chat.Api;
 using System;
 
 namespace AltVTutorial.GameEvents.Account
@@ -8,53 +7,50 @@ namespace AltVTutorial.GameEvents.Account
     internal class Login
     {
         [ClientEvent("Event.Login")]
-        public void OnPlayerLogin(TPlayer.TPlayer tplayer, String name, String password)
+        public void OnPlayerLogin(TPlayer.TPlayer player, String name, String password)
         {
-            if (tplayer.LoggedIn) return; // Bereits eingeloggt? Abbruch!
+            if (player.LoggedIn) return; // Bereits eingeloggt? Abbruch!
             if (name.Length < 3) return; // Username weniger als 3 Zeichen? Abbruch!
             if (password.Length < 5) return; // Passwort zu kurz? Abbruch!
 
             Logic.Account AccountHandle = new Logic.Account(name);
             if (!AccountHandle.DoesUserExist())
             {
-                tplayer.Emit("SendErrorMessage", "Es wurde kein Account mit dem Namen gefunden!");
+                player.Emit("SendErrorMessage", "Es wurde kein Account mit dem Namen gefunden!");
                 return;
             }
 
-            if (Datenbank.PasswortCheck(name, password))
+            if (!AccountHandle.Login(password))
             {
-                tplayer.Username = name;
-                Datenbank.AccountLaden(tplayer);
-                if (tplayer.positions[0] != 0.0 && tplayer.positions[1] != 0.0 && tplayer.positions[2] != 0.0)
-                {
-                    tplayer.Spawn(new AltV.Net.Data.Position(tplayer.positions[0], tplayer.positions[1], tplayer.positions[2]), 0);
-                    tplayer.Rotation = new AltV.Net.Data.Rotation(0, 0, tplayer.positions[3]);
-                }
-                else
-                {
-                    tplayer.Spawn(new AltV.Net.Data.Position(-425, 1123, 325), 0);
-                }
-                tplayer.Model = (uint)PedModel.FreemodeMale01;
-                tplayer.Emit("CloseLoginHud");
-                tplayer.Health = 200;
-                Utils.UpdateMoneyHud(tplayer, tplayer.Money);
-                tplayer.Emit("updatePB", (int)TPlayer.TPlayer.ProgressBars.Healthbar, 1.0);
-                tplayer.Emit("updatePB", (int)TPlayer.TPlayer.ProgressBars.Hungerbar, 0.5);
-                tplayer.Emit("updatePB", (int)TPlayer.TPlayer.ProgressBars.Thirstbar, 0.3);
-                if (!Utils.IsPlayerWhitelisted(tplayer))
-                {
-                    tplayer.SendChatMessage("{#eb0e27}Du stehst nicht auf der Whitelist!");
-                    tplayer.Kick("Du stehst nicht auf der Whitelist");
-                    return;
-                }
-                tplayer.LoggedIn = true;
-                tplayer.SendChatMessage("{00c900}Erfolgreich eingeloggt!");
-                Alt.Emit("SaltyChat:EnablePlayer", tplayer);
-            }
-            else
+                player.Emit("SendErrorMessage", "Falsches Passwort eingeben oder ungültige Eingabe!");
+                return;
+            };
+
+            if (!Utils.IsPlayerWhitelisted(player))
             {
-                tplayer.Emit("SendErrorMessage", "Falsches Passwort eingeben oder ungültige Eingabe!");
+                player.Emit("SendErrorMessage", "Du stehst nicht auf der Whitelist!");
+                player.Kick("Du stehst nicht auf der Whitelist");
+                return;
             }
+
+            player.DBID = AccountHandle.LastQueriedUser.Id;
+            player.AdminLevel = AccountHandle.LastQueriedUser.AdminLevel;
+            player.Money = AccountHandle.LastQueriedUser.Money;
+            player.Fraktion = AccountHandle.LastQueriedUser.Fraktion;
+            player.Payday = AccountHandle.LastQueriedUser.Payday;
+
+            player.Model = (uint)PedModel.FreemodeMale01;
+            player.Spawn(new AltV.Net.Data.Position(AccountHandle.LastQueriedUser.PosX, AccountHandle.LastQueriedUser.PosY, AccountHandle.LastQueriedUser.PosZ), 0);
+            player.Rotation = new AltV.Net.Data.Rotation(0, 0, AccountHandle.LastQueriedUser.PosZ);
+
+            player.Emit("CloseLoginHud");
+            Utils.UpdateMoneyHud(player, player.Money);
+            player.Emit("updatePB", (int)TPlayer.TPlayer.ProgressBars.Healthbar, 1.0);
+            player.Emit("updatePB", (int)TPlayer.TPlayer.ProgressBars.Hungerbar, 0.5);
+            player.Emit("updatePB", (int)TPlayer.TPlayer.ProgressBars.Thirstbar, 0.3);
+            player.LoggedIn = true;
+            player.Emit("SendMessage", "Erfolgreich eingeloggt!");
+            Alt.Emit("SaltyChat:EnablePlayer", player);
         }
     }
 }
